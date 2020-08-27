3b37dbdc6b9a7612d0fb2d36e34f94cc21b5dec9
johnsmith1500 <jv20052003@gmail.com>
2020-08-28 08:17:47 +0900

    Criação de uma tela de Listagem da previsão do tempo das regiões \(3 dias\). Término do código java \(tempo.js\)

--- www/components/tempo.js
+++ www/components/tempo.js
@@ -1,20 +1,25 @@
 // This is a JavaScript file
+$(document).on("click","#buscar", function(){
+  $(location).attr("href","listar.html");
+});
+
 
 $(document).on("click","#buscar",function(){
   $.ajax({
     type:"get",//como you enviar
     url:"http://apiadvisor.climatempo.com.br/api/v1/forecast/region/"+$("#regiao").val()+"?token=e45005750837d09bb0498c1405e6de94",//para onde enviar
-    sucess: function(data){//executar se der certo
-      var conteudo;
-      conteudo = "<p><strong>Região: </strong>"+data.region+"</p>";
-      conteudo += "<p><strong>Data: </strong>"+data.date_br+"</p>";
-      conteudo += "<p>"+data.image+"</p>";
-      conteudo += "<p>"+data.text+"</p>";
-
-      $("#dados").html(conteudo);
+    sucess: function listar(record){//executar se der certo
+      var Lista = "";
+      $.each(record.data, function(i,dados){
+        Lista += "<p><strong>Região: </strong>"+dados.region+"</p>";
+        Lista += "<p><strong>Data: </strong>"+dados.date_br+"</p>";
+        Lista += "<p>"+dados.image+"</p>";
+        Lista += "<p>"+dados.text+"</p>";
+      });
+      $("#listarDados").html(Lista);
     },
-    error: function(){//executar se der erro
-      alert("deu erro:");
-    }
+    error: function(data){
+        navigator.notification.alert("Erro ao buscar registro");
+      }
   });
 });
